3ec7d2dbe14b2022923648424c075337436b9c33
yoonseo31 <yoonseopark31@gmail.com>
2021-12-23 22:35:09 +0900

    첫 코딩

---
+++ practice1223.c
@@ -0,0 +1,48 @@
+// 첫 예제. 헬로월드 출력.
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     printf("Hello world \n");
+//     return 0;
+// }
+
+// 두 번째 예제. 연산자 출력.
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int num;
+//     num=3+4;
+//     printf("%d",num);
+//     return 0;
+// }
+
+// 세 번째 예제. 연산자 연습.
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     int num;    // num이라는 이름의 변수선언
+//     num=20;     // 변수 num에 20 저장
+//     printf("%d", num); // 변수 num의 값 참조
+//     return 0;
+// }
+
+// 다 섯번째 예제. 변수의 다양한 선언 및 초기화 방법.
+
+#include <stdio.h>
+
+int main(void)
+{
+    int num1=-858993460, num2=-858993460; //변수 num1, num2의 선언
+    int num3=30, num4=40;   //변수 num3, num4의 선언 및 초기화
+
+    printf("num1: %d, num2: %d \n", num1, num2);
+    num1=10;    // 변수 num1의 초기화
+    num2=20;    // 변수 num2의 초기화
+
+    printf("num1: %d, num2: %d \n", num1, num2);
+    printf("num3: %d, num4: %d \n", num3, num4);
+    return 0;
+}
